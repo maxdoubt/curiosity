42bb2b54dc3ca4bce475c8e6fe6a93269cd6a07a
Maxdoubt <benjamin.racher@gmail.com>
2014-01-22 18:59:49 -0600

    rock n roll

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Curriositree::Application.config.secret_key_base = 'e398ba5b6dc3cb15b91a120e11682a76850da72bf609886051061e04fea598ce7a93deeeedf13b387e90012c0b46f7cb3e8a8a7acad0555591020c449ffabbcc'
